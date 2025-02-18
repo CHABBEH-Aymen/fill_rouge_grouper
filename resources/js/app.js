0cb44c2fbf10a5ba16cb54276646b9f891a3da4a
ChabbehAymen <devaymen15.5.3@gmail.com>
2025-02-18 23:25:07 +0100

    added prime/breez

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,19 @@
 import './bootstrap';
 
 import Alpine from 'alpinejs';
+import PrimeVue from 'primevue/config';
+import {createApp} from 'vue';
+import App from './App.vue';
+import router from './router.js';
 
+
+createApp(App)
+	.use(router)
+	.use(PrimeVue, {
+		theme: {
+	        preset: Aura
+	    }
+	})
+	.mount('#app');
 window.Alpine = Alpine;
-
 Alpine.start();
